89cbc0e95b7f5e81bbce5748c74a9f8326db46dd
Jonathan Lisic <jonathanlisic@city17.bunnyland>
2013-09-28 10:26:00 -0400

    finished script

--- cdlFunc.R
+++ cdlFunc.R
@@ -2,36 +2,38 @@ library(sp)
 library(rgdal)
 library(raster)
 
+#windowDir <- 'C:/Documents and Settings/lisijo/My Documents/Data/'
+macDir <- '/Users/jonathanlisic/data/CDL/'
+#linuxDir <- '/Rproj/substrata/data/CDL/'
+
+
+Dir <- macDir
+years <- 1997:2012
+State <- 'NorthDakota'
+Fips <-  38
 
 source('~/src/cdlTools/getCDL.R')
 source('~/src/cdlTools/incident.R')
 source('~/src/cdlTools/rasterClusterApply.R')
 
+source('~/src/cdlTools/rasterProper.R')
+
+
+CDLRaster <- cdlBrick(Dir, Years, State, Fips) 
+
+writeRaster( CDLRaster, sprintf('%sCDL_%s/CDL_Brick_%d.grd',Dir,State,Fips),format="raster" )
 
 # set the appropriate directory
-
-
-#windowDir <- 'C:/Documents and Settings/lisijo/My Documents/Data/'
-macDir <- '/Volumes/data/CDL/'
-linuxDir <- '/Rproj/substrata/data/CDL/'
-
-
-Dir <- linuxDir
-years <- 2000:2012
-State <- 'Iowa'
-Fips <- 19
-
-# at this point we have a raster brick
-# CDLRaster <- brick( unlist( a ) ) 
-
 beginCluster()
 
   win <- 5
   subsets <- list( cultivated, corn, soybeans, winterWheat, springWheat, durumWheat, cotton, pasture )  
 
-  rasterClusterApply( CDLRaster, cellFun=incident, needed= list('win', 'subsets'), progress="text" )
+  raster.cdlIncident <- rasterClusterApply( CDLRaster, cellFun=incident, needed= list('win', 'subsets'), progress="text" )
 
 endCluster()
+
+writeRaster( raster.cdlIncident , sprintf('%sCDL_%s/CDL_Brick_Incident_%d.grd',Dir,State,Fips),format="raster" )
 
 
 
