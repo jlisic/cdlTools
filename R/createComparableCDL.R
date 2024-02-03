3f77c42229cd016143a693cbe74663b2896fce1e
Jonathan Lisic <jlisic@gmail.com>
2024-02-03 10:02:52 -0500

    fixed terra issues with create Comparable

--- R/createComparableCDL.R
+++ R/createComparableCDL.R
@@ -9,9 +9,10 @@
 #' @param filenames An array of file names of raster images to coerce into a raster list, 
 #'  if \code{rasterList} is not provided.
 #' @param baseIndex The index of the raster list element that all other elements will 
-#'  match with respect to resolution, projection and extent.
+#'  match with respect to resolution, projection and extent (default = 1).
 #' @param progress A string for the raster progress bar type, default "" is none, 
-#'  "text" provides text output, "window" provides a gui window if available.
+#'  "text" provides text output, "window" provides a gui window if available.  Not 
+#'  available for terra.
 #' @return A list of raster images matching in extent, resolution, and projection. 
 #' @examples
 #' \dontrun{
@@ -24,47 +25,67 @@
 #' @importFrom raster raster extent xres yres resample 
 #' @export
 createComparableCDL <-
-function( rasterList, filenames,  baseIndex, progress="") {
-
+function( rasterList, filenames,  baseIndex=1, progress="", ...) {
+  
+  # determine what type of object we are dealing with
+  if( class(rasterList[[1]])[1] == 'RasterLayer') {
+    type = 'raster'
+    raster_resample = function(x,y) raster::resample(x,y, method="ngb", progress=progress,...)  
+    raster_raster = raster::raster
+    raster_extent = raster::extent
+    raster_yres = raster::yres
+    raster_xres = raster::xres
+  } else {
+    type = 'terra'
+    raster_resample = function(x,y) terra::resample(x,y, method="near",...)  
+    raster_raster = terra::rast
+    raster_extent = function(x){return(
+      c(terra::xmin(x),terra::xmax(x),terra::ymin(x),terra::ymax(x))
+    )}
+    raster_yres = terra::yres
+    raster_xres = terra::xres
+  }
+  
 
   # take care of the case where the filenames are provided instead of a rasterlist
   if( !missing(filenames) ) {
     if( length(filenames) < baseIndex ) stop("baseIndex exceeds filenames length")
     rasterList <- list()
     for( i in 1:length(filenames)) {
-      rasterList[i] <- raster::raster(filenames[[i]]) 
+      rasterList[i] <- raster_raster(filenames[[i]]) 
     }
   } else {
     if( length(rasterList) < baseIndex ) stop("baseIndex exceeds rasterList length")
   }
   
-  # now that we have a list of raster files we will use the base index proprties for
+  # now that we have a list of raster files we will use the base index properties for
   # any possible resampling
   
-  base.extent <- raster::extent(rasterList[[baseIndex]])
-  base.xres <- raster::xres(rasterList[[baseIndex]])
-  base.yres <- raster::yres(rasterList[[baseIndex]])
+  base.extent <- raster_extent(rasterList[[baseIndex]])
+  base.xres <- raster_xres(rasterList[[baseIndex]])
+  base.yres <- raster_yres(rasterList[[baseIndex]])
 
-  rasterList.comparable <- list()
+  rasterList.comparable <- NULL 
 
   for( i in 1:length(rasterList) ) {
     if ( 
-        ( raster::extent(rasterList[[i]]) != base.extent ) & 
-        ( raster::xres(rasterList[[i]])   != base.xres ) & 
-        ( raster::yres(rasterList[[i]])   != base.yres )  
+        ( !identical(raster_extent(rasterList[[i]]), base.extent) ) | 
+        ( raster_xres(rasterList[[i]])   != base.xres ) | 
+        ( raster_yres(rasterList[[i]])   != base.yres )  
        ) {
 
       # resample
-      rasterList.comparable[i] <- raster::resample(
+      rasterList.comparable = append( rasterList.comparable,  raster_resample(
         x=rasterList[[i]], 
-        y=rasterList[[baseIndex]], 
-        method="ngb",
-        progress=progress ) 
+        y=rasterList[[baseIndex]]
+        ))
 
     } else {
-      rasterList.comparable[i] <- rasterList[[i]]
+      rasterList.comparable = append( rasterList.comparable, rasterList[[i]])
     }
   }
+  
+  if( type == 'terra') terra::varnames(rasterList.comparable) = names(rasterList)
 
   return(rasterList.comparable)
 }
--- R/getCDL.R
+++ R/getCDL.R
@@ -10,14 +10,14 @@
 #'@param https An optional boolean to turn on and off https, default is on.
 #'@param ssl.verifypeer An optional boolean to turn on and off ssl verfication, default is on.
 #'@param returnType An optional parameter to select to return either 'raster' or 'terra' based raster files.
-#'@return A list of CDL raster objects of interested county for a set of years.
+#'@return A list of CDL raster objects of interested county for a set of years.  Note that this is a generic list allowing for rasters with different extents and resolutions. 
 #'@examples
 #'\dontrun{
 #'# Get data for California, 2013 and 2015
 #'# by FIPS
 #'getCDL(6,c(2013,2015))
-#'# Get data for California, 2013 and 2015
-#'getCDL("California",c(2013,2015))
+#'# Get data for California, 2013 and 2015 with Terra
+#'getCDL("California",c(2013,2015), returnType='terra')
 #'# Get all the west coast from 2009 to 2016
 #'getCDL(c("CA","OR","WA"),2013:2016)
 #'}
@@ -41,31 +41,38 @@ getCDL <- function(x,year,alternativeUrl,location=tempdir(),https=TRUE, ssl.veri
   names.array <- c()
  
   x <- fips(x)
-  years <- year # fix but can't change due to legacy
+  cdl_years <- year # fix but can't change due to legacy
 
   for( i in 1:length(x) ) {
     if( is.na(x[i]) ) next 
 
-    for(year in years){
+    for(cdl_year in cdl_years){
       out_target <- paste(location, 
                           sprintf("CDL_%d_%02d.tif", 
-                                  year, x[i]), sep = "/")
+                                  cdl_year, x[i]), sep = "/")
       if(file.exists(out_target)){
         target   <- raster_read(out_target)
-        cdl.list <- append(cdl.list, target) 
+        if( length(cdl.list) == 0 ) {
+          cdl.list = list(target) 
+        } else {
+          cdl.list[[length(cdl.list) + 1]] = target 
+        }
         next
       }
       # create cropscape URL 
       if(missing(alternativeUrl)) {
           if(https) {
-            url <- sprintf("https://nassgeodata.gmu.edu/cdlservicedata/nass_data_cache/byfips/CDL_%d_%02d.zip",year,x[i]) 
+            url <- sprintf("https://nassgeodata.gmu.edu/cdlservicedata/nass_data_cache/byfips/CDL_%d_%02d.zip",cdl_year,x[i]) 
           } else {
-            url <- sprintf("http://nassgeodata.gmu.edu/cdlservicedata/nass_data_cache/byfips/CDL_%d_%02d.zip",year,x[i]) 
+            url <- sprintf("http://nassgeodata.gmu.edu/cdlservicedata/nass_data_cache/byfips/CDL_%d_%02d.zip",cdl_year,x[i]) 
           }
       } else {
-        url <- paste(alternativeUrl,sprintf("CDL_%d_%02d.tif",year,x[i]),sep="/")
+        url <- paste(alternativeUrl,sprintf("CDL_%d_%02d.tif",cdl_year,x[i]),sep="/")
       }
        
+      # useful test URL
+      # "http://nassgeodata.gmu.edu/cdlservicedata/nass_data_cache/byfips/CDL_2006_19.zip"
+      
       # check if URL exists
       if(httr::http_error(url, config = httr::config(ssl_verifypeer = ssl.verifypeer)) ){
         warning( sprintf("%s does not exist.",url) )
@@ -76,21 +83,28 @@ getCDL <- function(x,year,alternativeUrl,location=tempdir(),https=TRUE, ssl.veri
       if(missing(alternativeUrl) ) {
         httr::GET(url, 
           httr::write_disk(paste(
-            location, sprintf("CDL_%d_%02d.zip", year, x[i]), sep = "/"), 
+            location, sprintf("CDL_%d_%02d.zip", cdl_year, x[i]), sep = "/"), 
           overwrite = TRUE), 
           config = httr::config(ssl_verifypeer = ssl.verifypeer), httr::progress())
         
         utils::unzip(paste(
-          location, sprintf("CDL_%d_%02d.zip", year, x[i]), sep="/"), 
+          location, sprintf("CDL_%d_%02d.zip", cdl_year, x[i]), sep="/"), 
           exdir = location)
         
-        unlink(paste(location,sprintf("CDL_%d_%02d.zip",year,x[i]),sep="/"))
+        unlink(paste(location,sprintf("CDL_%d_%02d.zip",cdl_year,x[i]),sep="/"))
       } else {
-        utils::download.file(url,destfile=paste(location,sprintf("CDL_%d_%02d.tif",year,x[i]),sep="/"),mode="wb", method = "curl")
+        utils::download.file(url,destfile=paste(location,sprintf("CDL_%d_%02d.tif",cdl_year,x[i]),sep="/"),mode="wb", method = "curl")
       }
-      target <- raster_read(paste(location,sprintf("CDL_%d_%02d.tif",year,x[i]),sep="/")) 
-      names.array <- append(names.array, paste0( fips(x[i],to="Abbreviation"),year))
-      cdl.list <- append(cdl.list,target)
+      target <- raster_read(paste(location,sprintf("CDL_%d_%02d.tif",cdl_year,x[i]),sep="/")) 
+      names.array <- append(names.array, paste0( fips(x[i],to="Abbreviation"),cdl_year))
+      # terra seems to overwrite the append function
+      # so we do this to allow extents to be in the same list even
+      # if they don't match
+      if( length(cdl.list) == 0 ) {
+        cdl.list = list(target) 
+      } else {
+        cdl.list[[length(cdl.list) + 1]] = target 
+      }
     }
   }
   names(cdl.list) <- names.array
